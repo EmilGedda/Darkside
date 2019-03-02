f5ae72c70ad4f4ee53513075d65841791040a5cc
Emil Gedda <emil.gedda@emilgedda.se>
2019-03-02 16:12:41 +0100

    fix linter issues

--- test/sum.ts
+++ test/sum.ts
@@ -1,9 +1,9 @@
 import { sum } from '../src/sum';
 
 test('basic', async () => {
-  expect(sum()).toBe(0);
+    expect(sum()).toBe(0);
 });
 
-test('basic again',  async () => {
-  expect(sum(1, 2)).toBe(3);
+test('basic again', async () => {
+    expect(sum(1, 2)).toBe(3);
 });
