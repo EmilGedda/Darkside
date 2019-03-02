9ebead068526f7f4ea04c8cea19b9b40db7aebdc
Emil Gedda <emil.gedda@emilgedda.se>
2019-03-02 16:06:04 +0100

    set up jest and add test example

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "roots": [
+    "test"
+  ],
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "verbose": true,
+  "testRegex": ".(t|j)sx?$",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+}
