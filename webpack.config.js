0a397ca19239542bf26056ad71a7e717769b7a14
Emil Gedda <emil.gedda@emilgedda.se>
2019-03-06 22:44:46 +0100

    fix sourcemaps

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './dist/app.js',
   mode: 'development',
+  devtool: 'source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
