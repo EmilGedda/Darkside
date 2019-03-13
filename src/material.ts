c70fb6acb09e55e07e7894ad52f4533871e7d72a
Anders Eriksson <aeri3@kth.se>
2019-03-13 18:33:33 +0100

    snell's law

--- src/material.ts
+++ src/material.ts
@@ -4,6 +4,7 @@ import materials from './data/materials.json';
  * Material name constants.
  */
 export enum MATERIAL {
+    VACUUM = 'VACUUM',
     SCHOTT_BK7 = 'SCHOTT_BK7',
     SCHOTT_BAF10 = 'SCHOTT_BAF10',
 }
@@ -12,6 +13,7 @@ export enum MATERIAL {
  * Material defined by Sellmeier coefficients.
  */
 export class Material {
+    public name: string;
     private b1: number;
     private b2: number;
     private b3: number;
@@ -20,6 +22,7 @@ export class Material {
     private c3: number;
 
     public constructor(material: MATERIAL = MATERIAL.SCHOTT_BK7) {
+        this.name = material;
         const coeffs = materials[material];
 
         this.b1 = coeffs[0];
@@ -34,11 +37,12 @@ export class Material {
      * Calculate the refractive index of the material for a given wavelength
      * using the Sellmeier equation.
      *
-     * @param wavelength Wavelength in vacuum
+     * @param wavelength Wavelength in vacuum in nm
      *
      * @returns The refractive index of the material for a given wavelength.
      */
     public refractiveIndex(wavelength: number): number {
+        wavelength /= 1000;
         const a = (this.b1 * wavelength ** 2) / (wavelength ** 2 - this.c1);
         const b = (this.b2 * wavelength ** 2) / (wavelength ** 2 - this.c2);
         const c = (this.b3 * wavelength ** 2) / (wavelength ** 2 - this.c3);
--- test/wavelength.ts
+++ test/wavelength.ts
@@ -1,8 +1,27 @@
 import { Wavelength } from '../src/wavelength';
+import { Material, MATERIAL } from '../src/material';
 
 test('Wavelength construction', async () => {
     let wl = new Wavelength(500);
     expect(wl.value).toBe(500);
+});
+
+test('Wavelength.refract(from: Material, to: Material, incision: number): number', async () => {
+    let wl = new Wavelength(380);
+    let vacuum = new Material(MATERIAL.VACUUM);
+    let bk7 = new Material(MATERIAL.SCHOTT_BK7);
+    let baf10 = new Material(MATERIAL.SCHOTT_BAF10);
+
+    expect(wl.refract(bk7, baf10, Math.PI / 4)).toBeCloseTo(0.6372978023775094);
+    expect(wl.refract(bk7, baf10, 0)).toBe(0);
+    expect(wl.refract(baf10, bk7, Math.PI / 4)).toBeCloseTo(0.7845625673502952);
+    expect(wl.refract(baf10, bk7, 0)).toBe(0);
+    expect(wl.refract(baf10, bk7, Math.PI / 8)).toBeCloseTo(0.4246022017706678);
+    expect(wl.refract(vacuum, bk7, Math.PI / 4)).toBeCloseTo(0.4609263517582702);
+    
+    expect(wl.refract(bk7, bk7, Math.PI / 4)).toBeCloseTo(Math.PI / 4);
+    expect(wl.refract(baf10, baf10, Math.PI / 4)).toBeCloseTo(Math.PI / 4);
+    expect(wl.refract(vacuum, vacuum, Math.PI / 4)).toBeCloseTo(Math.PI / 4);
 });
 
 test('Wavelength.toXYZ()', async () => {
