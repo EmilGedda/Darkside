7644a027c6d2b3e1b5cc0486670abee7627c7d45
Anders Eriksson <aeri3@kth.se>
2019-03-15 15:09:46 +0100

    add Schott SF10 glass (#15)

--- test/material.ts
+++ test/material.ts
@@ -15,6 +15,7 @@ test('Material construction', async () => {
 test('Material.refractiveIndex(lambda: number): number', async () => {
     const bk7 = new Material(MATERIAL.SCHOTT_BK7); // Schott BK7 glass
     const baf10 = new Material(MATERIAL.SCHOTT_BAF10); // Schott BAF10 glass
+    const sf10 = new Material(MATERIAL.SCHOOTT_SF10); // Schott SF10 flint glass
 
     expect(bk7.refractiveIndex(430)).toBeCloseTo(1.5273);
     expect(bk7.refractiveIndex(600)).toBeCloseTo(1.5163);
@@ -22,4 +23,7 @@ test('Material.refractiveIndex(lambda: number): number', async () => {
     expect(baf10.refractiveIndex(430)).toBeCloseTo(1.6891);
     expect(baf10.refractiveIndex(600)).toBeCloseTo(1.6692);
     expect(baf10.refractiveIndex(770)).toBeCloseTo(1.661);
+    expect(sf10.refractiveIndex(430)).toBeCloseTo(1.7643);
+    expect(sf10.refractiveIndex(600)).toBeCloseTo(1.7267);
+    expect(sf10.refractiveIndex(770)).toBeCloseTo(1.7128);
 });
